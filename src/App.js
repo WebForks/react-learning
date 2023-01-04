1962e15901930604eed82d08c44bcca3ff21dd4b
a3eon <35718739+a3eon-1@users.noreply.github.com>
2023-01-03 21:08:26 -0800

    ombapi key

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 
-const API_URL = API_KEY;
+const API_URL = 'http://www.omdbapi.com/?apikey=2897691b';
 
 const App = () => {
   const handleKeyDown = event => {
